855a7d75b194771fc329f58d49c6d49f18dd069c
P93699 <moch.nurfaizal@pegadaian.co.id>
2025-10-03 00:37:25 +0700

    [ORDER-SERVICE] - Perbaikan Throw Error User ID Tidak Ditemukan

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -10,8 +10,16 @@ export const createOrderHandler = async (req: Request, res: Response) => {
   try {
     const order = await createOrder(req.body);
     res.status(201).json(order);
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
+    // Check for user not found error
+    if (
+      error instanceof Error &&
+      error.message &&
+      error.message.toLowerCase().includes('invalid userid')
+    ) {
+      return res.status(404).json({ error: `Failed to create order. User ID [${req.body.userId}] not found!` });
+    }
     res.status(500).json({ error: 'Failed to create order' });
   }
 };
